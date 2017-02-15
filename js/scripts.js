0013bf3fc2bdbad2bfe48bf4266c41bb0b4a3fd5
Kaya Winn and Sara Schultz <sara@>
2017-02-15 15:48:45 -0800

    add radio buttons and location option.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+
+
+
     var name = $("input#name").val();
     var service = $("input#service").val();
     var date = $("input#date").val();
     var start = $("input#start").val();
     var end = $("input#end").val();
-    console.log(end);
-
+    // function validate() {
+    //   if (start == null) {
+    //   alert("Time must be filled out");
+    //     // return false;
+    //   }}
     $(".name").text(name);
     $(".service").text(service);
     $(".date").text(date);
