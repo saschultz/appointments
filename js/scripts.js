42874db5508ecab238344d2f307b89f39c9e520a
Kaya Winn and Sara Schultz <sara@>
2017-02-15 16:51:41 -0800

    adds updated formatting, iframe with map

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
     $(".date").text(date);
     $(".start").text(start);
     $(".end").text(end);
+    $(".map").text( src="https://www.google.com/maps/place/445+SE+39th+Ave,+Portland,+OR+97214/@45.5197872,-122.6252259,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0ebbe4ae0ed:0x6b29c59b525b3b72!8m2!3d45.5197835!4d-122.6230319" );
     $("#review").show();
 
     event.preventDefault();
