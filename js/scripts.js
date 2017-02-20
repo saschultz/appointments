605dfd055789b7e078d15a7297190a44348be149
Sara Schultz <sara@>
2017-02-20 11:53:20 -0800

    add array for all input ids and looping

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    // debugger;
+    var blanks = ["name", "service", "date", "start", "end"];
 
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput);
+    });
 
-
-    var name = $("input#name").val();
-    var service = $("input#service").val();
-    var date = $("input#date").val();
-    var start = $("input#start").val();
-    var end = $("input#end").val();
-    // function validate() {
-    //   if (start == null) {
-    //   alert("Time must be filled out");
-    //     // return false;
-    //   }}
-    $(".name").text(name);
-    $(".service").text(service);
-    $(".date").text(date);
-    $(".start").text(start);
-    $(".end").text(end);
-    $(".map").text( src="https://www.google.com/maps/place/445+SE+39th+Ave,+Portland,+OR+97214/@45.5197872,-122.6252259,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0ebbe4ae0ed:0x6b29c59b525b3b72!8m2!3d45.5197835!4d-122.6230319" );
+    // console.log(userInput);
+    // var name = $("input#name").val();
+    // var service = $("input#service").val();
+    // var date = $("input#date").val();
+    // var start = $("input#start").val();
+    // var end = $("input#end").val();
+    // // function validate() {
+    // //   if (start == null) {
+    // //   alert("Time must be filled out");
+    // //     // return false;
+    // //   }}
+    // $(".name").text(name);
+    // $(".service").text(service);
+    // $(".date").text(date);
+    // $(".start").text(start);
+    // $(".end").text(end);
+    // $(".map").text( src="https://www.google.com/maps/place/445+SE+39th+Ave,+Portland,+OR+97214/@45.5197872,-122.6252259,17z/data=!3m1!4b1!4m5!3m4!1s0x5495a0ebbe4ae0ed:0x6b29c59b525b3b72!8m2!3d45.5197835!4d-122.6230319" );
     $("#review").show();
 
     event.preventDefault();
